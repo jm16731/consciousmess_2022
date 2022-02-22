999388e638036456ceafc087f2c71f93320d182c
jmurrel1 <jmurrel1@cs6242-vm03.uwg.westga.edu>
2022-02-22 17:12:02 -0500

    create ripple, redirect to home

--- app/models/consciousmess.rb
+++ app/models/consciousmess.rb
@@ -1,3 +1,3 @@
 class Consciousmess < ApplicationRecord
-  has_many :ripple, dependent: :destroy
+  #has_many :ripple, dependent: :destroy
 end
--- app/models/ripple.rb
+++ app/models/ripple.rb
@@ -1,5 +1,5 @@
 class Ripple < ApplicationRecord
-  belongs_to :consciousmess
+  #belongs_to :consciousmess
 
   validates :name, :posted, :message, presence: true
   #validates :url, allow_blank: true, url: true
